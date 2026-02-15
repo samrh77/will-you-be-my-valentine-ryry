94ecdc200ac61b240c6a81159ebe09299af7009c
samrh77 <eksam4515@gmail.com>
2026-02-14 19:12:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+//elements
+const envelope = document.getElementById("envelope-container")
+const letter = document.getElementById("letter-container")
+const noBtn = document.querySelector(".no-btn")
+const yesBtn = document.querySelector(".yes-btn")
+
+const title = document.getElementById("letter-tittle")
+const catImg = document.getElementById("letter-cat")
+const buttons = document.getElementById("letter-buttons")
+const finalText = document.getElementById("final-text")
+
+// click Envelope
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout( () => {
+        document.querySelector(".letter-window").classList.add("open");
+    },50);
+})
+
+//Logic to make Yes btn to grow
+
+let yesScale = 1;
+
+yesBtn.style.position = "relative"
+yesBtn.style.transformOrigin= "center center"
+yesBtn.style.transition = "transform 0.3s ease";
+
+noBtn.addEventListener("click", () => {
+    yesScale += 2;
+
+    if (yesBtn.style.position !== "fixed") {
+        yesBtn.style.position = "fixed";
+        yesBtn.style.top = "50%";
+        yesBtn.style.left = "50%";
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }else{
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }
+    });
+
+    //Yes is clicked
+
+    yesBtn.addEventListener("click",() => {
+        title.textContent = "Yippeee!";
+
+        catImg.src = "cat_dance.gif";
+
+        document.querySelector(".letter-window").classList.add("final");
+
+        buttons.style.display = "none";
+
+        finalText.style.display = "block";
+    });
